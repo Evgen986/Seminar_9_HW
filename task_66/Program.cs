a26ae7a459c37e2502cd840ffc767d995506349c
Evgenyi_Malyutin <Evgen986@mail.run>
2022-11-11 11:28:00 +0300

    added HW_Seminar_9

---
+++ task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+            M = 1; N = 15 -> 120
+            M = 4; N = 8. -> 30
+*/
+Main();
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите число №1: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Введите число №2: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine(SumNumbersRec(m, n));
+}
+
+
+int SumNumbersRec(int m, int n)
+{
+    if (n < m) return 0;
+    else return n + SumNumbersRec(m, n - 1);
+}
---
+++ task_68/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+              m = 2, n = 3 -> A(m,n) = 9
+              m = 3, n = 2 -> A(m,n) = 29
+*/
+Main();
+
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите число №1: ");
+    int m = int.Parse(Console.ReadLine());
+    Console.Write("Введите число №2: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine(AkkermanRec(m, n));
+}
+
+
+int AkkermanRec(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AkkermanRec(m - 1, 1);
+    if (m > 0 && n > 0) return AkkermanRec(m - 1, AkkermanRec(m, n - 1));
+    return AkkermanRec(m, n);
+}
